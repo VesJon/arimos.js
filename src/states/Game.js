6179565970678d3a59e65b0ba92e8b1bc81d432b
Vesna Tan <tanvezi@gmail.com>
2018-01-18 01:20:49 -0500

    basic-layout

--- src/states/Game.js
+++ src/states/Game.js
@@ -1,35 +1,55 @@
 /* globals __DEV__ */
 import Phaser from 'phaser'
-import Mushroom from '../sprites/Mushroom'
+// import Knight from '../sprites/knight'
 
 export default class extends Phaser.State {
   init () {}
   preload () {}
-
   create () {
-    const bannerText = 'Phaser + ES6 + Webpack'
-    let banner = this.add.text(this.world.centerX, this.game.height - 80, bannerText, {
-      font: '40px Bangers',
-      fill: '#77BFA3',
-      smoothed: false
-    })
-
-    banner.padding.set(10, 16)
-    banner.anchor.setTo(0.5)
-
-    this.mushroom = new Mushroom({
-      game: this.game,
-      x: this.world.centerX,
-      y: this.world.centerY,
-      asset: 'mushroom'
-    })
-
-    this.game.add.existing(this.mushroom)
+    this.game.physics.startSystem(Phaser.Physics.ARCADE) // has to be called in Game.js ???? whyy tho
+    this.map = this.game.add.tilemap('tilemap')
+    this.map.addTilesetImage('tiles', 'tiles')
+    this.groundLayer = this.map.createLayer('TileLayer')// must match layer name
+    this.groundLayer.scale.setTo(this.game.width / (32 * 15), this.game.height / (32 * 10))// resizes tile map to fit
+    this.groundLayer.resizeWorld()
+    // this.cursors = this.game.input.keyboard.createCursorKeys()
+    // this.setupKnight()
+    this.p = this.game.add.sprite(32, 32, 'knight')
+    this.game.physics.enable(this.p)
+    this.p.body.collideWorldBounds = true
+    this.cursors = this.game.input.keyboard.createCursorKeys()
   }
+  // setupKnight () {
+  //   this.knight = new Knight(
+  //     this.game,
+  //     this.game.world.centerX,
+  //     this.game.world.height - 100
+  //   )
+  //   this.game.add.existing(this.paddle)
+  // }
+  update () {
+    this.game.physics.arcade.collide(this.p, this.groundLayer)
+    this.p.body.velocity.x = 0
+    this.p.body.velocity.y = 0
+    if (this.cursors.down.isDown) {
+      this.p.body.velocity.y = 200
+    }
+    if (this.cursors.up.isDown) {
+      this.p.body.velocity.y = -200
+    }
+    if (this.cursors.left.isDown) {
+      this.p.body.velocity.x = -150
+    }
+    else if (this.cursors.right.isDown)
+    {
+        this.p.body.velocity.x = 150
 
+    }
+
+}
   render () {
     if (__DEV__) {
-      this.game.debug.spriteInfo(this.mushroom, 32, 32)
+      this.game.debug.spriteInfo(this.groundLayer, 32, 32)
     }
   }
 }
--- src/states/Splash.js
+++ src/states/Splash.js
@@ -5,15 +5,15 @@ export default class extends Phaser.State {
   init () {}
 
   preload () {
-    this.loaderBg = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'loaderBg')
-    this.loaderBar = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'loaderBar')
-    centerGameObjects([this.loaderBg, this.loaderBar])
+    // this.loaderBg = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'loaderBg')
+    // this.loaderBar = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'loaderBar')
+    // centerGameObjects([this.loaderBg, this.loaderBar])
 
-    this.load.setPreloadSprite(this.loaderBar)
+    // this.load.setPreloadSprite(this.loaderBar)
     //
     // load your assets
     //
-    this.load.image('mushroom', 'assets/images/mushroom2.png')
+    // this.load.image('mushroom', 'assets/images/mushroom2.png')s
   }
 
   create () {
