0a36cfbfdbb612de22767252b98b28aed0c25f2b
Vesna Tan <tanvezi@gmail.com>
2018-01-18 11:13:28 -0500

    intial commit

--- src/states/Game.js
+++ src/states/Game.js
@@ -12,21 +12,12 @@ export default class extends Phaser.State {
     this.groundLayer = this.map.createLayer('TileLayer')// must match layer name
     this.groundLayer.scale.setTo(this.game.width / (32 * 15), this.game.height / (32 * 10))// resizes tile map to fit
     this.groundLayer.resizeWorld()
-    // this.cursors = this.game.input.keyboard.createCursorKeys()
-    // this.setupKnight()
-    this.p = this.game.add.sprite(32, 32, 'knight')
+    this.p = this.game.add.sprite(200, 200, 'knight')
+  
     this.game.physics.enable(this.p)
     this.p.body.collideWorldBounds = true
     this.cursors = this.game.input.keyboard.createCursorKeys()
   }
-  // setupKnight () {
-  //   this.knight = new Knight(
-  //     this.game,
-  //     this.game.world.centerX,
-  //     this.game.world.height - 100
-  //   )
-  //   this.game.add.existing(this.paddle)
-  // }
   update () {
     this.game.physics.arcade.collide(this.p, this.groundLayer)
     this.p.body.velocity.x = 0
@@ -39,17 +30,13 @@ export default class extends Phaser.State {
     }
     if (this.cursors.left.isDown) {
       this.p.body.velocity.x = -150
+    } else if (this.cursors.right.isDown) {
+      this.p.body.velocity.x = 150
     }
-    else if (this.cursors.right.isDown)
-    {
-        this.p.body.velocity.x = 150
-
-    }
-
-}
+  }
   render () {
     if (__DEV__) {
-      this.game.debug.spriteInfo(this.groundLayer, 32, 32)
+      this.game.debug.spriteInfo(this.p, 32, 32)
     }
   }
 }
