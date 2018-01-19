05f9916181ae364af5e784d7576a6cc50decfaab
Vesna Tan <tanvezi@gmail.com>
2018-01-18 23:04:41 -0500

    added sword direction

--- src/sprites/player.js
+++ src/sprites/player.js
@@ -12,26 +12,26 @@ export default class Player extends Phaser.Sprite {
   update () {
     this.body.velocity.x = 0
     this.body.velocity.y = 0
+    this.sword = this.game.add.sprite(0, 0, 'sword')
+    this.addChild(this.sword)
     if (this.cursors.down.isDown) {
       this.body.velocity.y = 200
+      this.sword.anchor.setTo(2, 2.5) // down sword
+      this.sword.angle+=180
     }
     if (this.cursors.up.isDown) {
       this.body.velocity.y = -200
+      this.sword.anchor.setTo(-2, 0.5) // top sword
+
     }
     if (this.cursors.left.isDown) {
       this.body.velocity.x = -150
+      this.sword.anchor.setTo(3, 0.5) // left sword
+      this.sword.angle-=90 // wasd
     } else if (this.cursors.right.isDown) {
       this.body.velocity.x = 150
+      this.sword.anchor.setTo(-3, 2) // right sword
+      this.sword.angle+=90
     }
-    this.sword = this.game.add.sprite(0, 0, 'sword')
-    // this.player.sword.anchor.setTo(-3, 2) // right sword
-    //this.player.sword.angle+=90
-    // this.player.sword.anchor.setTo(-2, 0.5) // top sword
-    // this.player.sword.angle
-    // this.player.sword.anchor.setTo(3, 1) // left sword
-    // this.player.sword.angle-=90 // wasd
-    this.addChild(this.sword)
-
-
   }
 }
