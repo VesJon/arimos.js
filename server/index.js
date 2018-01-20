c3386bd69c91f4f05f9f11fb7dd9f79319211874
Jon Rea <jonarea@gmail.com>
2018-01-20 17:11:08 -0500

    setup server

--- server/index.js
+++ server/index.js
@@ -2,10 +2,10 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
-app.use(express.static(path.join(__dirname, '..', 'dist')))
+app.use(express.static(path.join(__dirname, '..', 'www')))
 
 app.get('*', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '..', 'index.html'))
+  res.sendFile(path.join(__dirname, '..', 'www/index.html'))
 })
 
 app.listen(3000)
